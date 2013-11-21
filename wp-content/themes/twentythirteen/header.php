2535e27dbbbe8b0206ae80c4c8a2eba5a6958d20
Simon Kerr <simon.p.kerr@gmail.com>
2013-11-21 22:34:38 +0000

    updates to style

--- wp-content/themes/twentythirteen/header.php
+++ wp-content/themes/twentythirteen/header.php
@@ -31,21 +31,21 @@
 </head>
 
 <body <?php body_class(); ?>>
-	<div id="page" class="hfeed site">
-		<header id="masthead" class="site-header" role="banner">
-			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-				<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+	<div id="page" class="hfeed container">
+		<header id="masthead" class="site-header row" role="banner">
+                    
+			<a class="h1 col-xs-8 col-sm-6" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+				<?php bloginfo( 'name' ); ?>
+				<small><?php bloginfo( 'description' ); ?></small>
 			</a>
-
-			<div id="navbar" class="navbar">
-				<nav id="site-navigation" class="navigation main-navigation" role="navigation">
-					<h3 class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></h3>
-					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-					<?php get_search_form(); ?>
-				</nav><!-- #site-navigation -->
-			</div><!-- #navbar -->
+			
+                        <nav id="site-navigation" class="navbar navbar-default" role="navigation">
+                                <a class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></a>
+                                <a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
+                                <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+                                <?php get_search_form(); ?>
+                        </nav><!-- #site-navigation -->
+                                
 		</header><!-- #masthead -->
 
-		<div id="main" class="site-main">
+		<div id="main" class="site-main row">
